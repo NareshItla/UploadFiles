b65984ed622b90a09fd3b1ad1fde5e55f1dfec8f
Aneesha <aneesha.n@way2online.co.in>
2019-03-02 19:52:52 +0530

    edited

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,77 +1,81 @@
-var UploadFile = angular.module('UploadApp',[]);
-   var socket;
-UploadFile.controller('uploadController',function($scope){
+var UploadFile = angular.module('UploadApp', []);
+var socket;
+UploadFile.controller('uploadController', function ($scope) {
     socket = io.connect('http://localhost:3000/');
-  
-    socket.on('connect',function(){
-	    console.log("connected")
-	})
-	
-	
+
+    socket.on('connect', function () {
+        console.log("connected")
+    })
+
+
 });
-UploadFile.directive("fileinput", [function($) {
+UploadFile.directive("fileinput", [function ($) {
     return {
-      link: function(scope, element, attributes) {
-          scope.sentPercent = 0 + '%';  
-        element.bind("change", function(changeEvent) {
-            
-          scope.fileinput = changeEvent.target.files[0];
-          var file = changeEvent.target.files[0];
-          var fileName = changeEvent.target.files[0].name;
-          var reader = new FileReader();
-          reader.readAsText(file);
-          reader.onload = function(loadEvent) {
-            scope.$apply(function() {
-            let stringToSplit = loadEvent.target.result;
-            const allLines = stringToSplit.split(/\r\n|\n/);
-          var j;
-         scope.status = true;
-         scope.pause = function(){
-            scope.status = false;
-         }
-         var sendLength;
-         scope.resume = function(){
-            scope.status = true;
-            run(j);
-         }
-        
-         var factor;
-         if(allLines.length && allLines.length>100){
-            factor = 100;
-            sendLength = Math.floor(allLines.length/factor);
-         }
-         else{
-            sendLength = 1;
-         }
-        function run(i){
-                j = i;
-                if(i >= allLines.length){
-                    scope.$apply(function(){scope.sentPercent = 100 + '%'});
-                }
-                else{
-                        socket.emit('message',allLines.slice(i,i+sendLength),fileName);
-                        i = i + sendLength
-                        socket.once('cb',function(msg){
-                        //console.log(msg)
-                        if(msg == 200){
-                            if(scope.status){
-                                scope.$apply(function(){
-                                scope.sentPercent = (i/allLines.length).toFixed(2)*100 + '%'
-                                         $("div.progress-bar").css("width", scope.sentPercent);
-                                run(i);
-                                }
-                        else{
-                            console.log("paused");
+        link: function (scope, element, attributes) {
+            scope.sentPercent = 0 + '%';
+            element.bind("change", function (changeEvent) {
+
+                scope.fileinput = changeEvent.target.files[0];
+                var file = changeEvent.target.files[0];
+                var fileName = changeEvent.target.files[0].name;
+                var reader = new FileReader();
+                reader.readAsText(file);
+                reader.onload = function (loadEvent) {
+                    scope.$apply(function () {
+                        let stringToSplit = loadEvent.target.result;
+                        const allLines = stringToSplit.split(/\r\n|\n/);
+                        var j;
+                        scope.status = true;
+                        scope.pause = function () {
+                            scope.status = false;
+                        };
+                        var sendLength;
+                        scope.resume = function () {
+                            scope.status = true;
+                            run(j);
+                        };
+
+                        var factor;
+                        if (allLines.length && allLines.length > 100) {
+                            factor = 100;
+                            sendLength = Math.floor(allLines.length / factor);
                         }
-                        }  
-                        })
+                        else {
+                            sendLength = 1;
+                        }
+                        function run(i) {
+                            j = i;
+                            if (i >= allLines.length) {
+                                scope.$apply(function () {
+                                    scope.sentPercent = 100 + '%'
+                                });
+                            }
+                            else {
+                                socket.emit('message', allLines.slice(i, i + sendLength), fileName);
+                                i = i + sendLength;
+                                socket.once('cb', function (msg) {
+                                    //console.log(msg)
+                                    if (msg == 200) {
+                                        if (scope.status) {
+                                            scope.$apply(function () {
+                                                scope.sentPercent = (i / allLines.length).toFixed(2) * 100 + '%'
+                                                $("div.progress-bar").css("width", scope.sentPercent);
+                                                run(i);
+                                            })
+                                        }
+                                        else {
+                                            console.log("paused");
+                                        }
+                                    }
+                                })
+                            }
+                        }
+
+                        run(0);
+                    });
                 }
-            }
-            run(0);
             });
-          }          
-        });
-      }
+        }
     }
-  }]);
+}]);
  
